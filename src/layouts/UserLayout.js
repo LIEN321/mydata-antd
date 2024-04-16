001b8e58dc63e56dedee3c2dc010d5f0419ac2e9
Lien <tle3212000@163.com>
2024-04-17 01:02:17 +0800

    feat: 更新页脚版本号0.8.1

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -27,7 +27,7 @@ const links = [
 
 const copyright = (
   <Fragment>
-    MyData v0.8.0 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
+    MyData v0.8.1 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
     <a
       key="github"
       title="git"
