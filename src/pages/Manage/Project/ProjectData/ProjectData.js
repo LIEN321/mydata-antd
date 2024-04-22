52d16f635fd4dca92dcd3b1a22ac77173f9d1332
Lien <tle3212000@163.com>
2024-04-22 13:26:19 +0800

    fix: 修复select未选中默认值问题

--- src/pages/Manage/Project/ProjectData/ProjectData.js
+++ src/pages/Manage/Project/ProjectData/ProjectData.js
@@ -106,7 +106,7 @@ class ProjectData extends PureComponent {
       <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
         <Col md={12} sm={24}>
           <span style={{ color: 'red' }}>*</span><span style={{ fontWeight: 'bold' }}>选择环境：</span>
-          {envList.length > 0 ? <Select placeholder="请选择所属环境" onChange={this.handleChangeEnv} style={{ width: 200 }} initialValue={envList.length > 0 ? envList[0].id : null}>
+          {envList.length > 0 ? <Select placeholder="请选择所属环境" onChange={this.handleChangeEnv} style={{ width: 200 }} defaultValue={envList.length > 0 ? envList[0].id : null}>
             {envList.map(e => (
               <Select.Option key={e.id} value={e.id}>
                 <Row style={{ width: '168px' }}>
