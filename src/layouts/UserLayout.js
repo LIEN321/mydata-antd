a12e87484f424460d76ae96a6a6a2d107b547380
Lien <tle3212000@163.com>
2024-04-25 09:40:38 +0800

    fix: 修复环境变量任务缺少produceMode参数问题

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -27,7 +27,7 @@ const links = [
 
 const copyright = (
   <Fragment>
-    MyData v0.8.1.1 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
+    MyData v0.8.1.2 Copyright <Icon type="copyright" /> 2024 LIEN{' '}
     <a
       key="github"
       title="git"
