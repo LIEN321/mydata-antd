4427ac1a0ee72a38148510b83b5ed62870bcd451
LIEN <tle3212000@163.com>
2024-12-03 15:59:16 +0800

    refactor: 调整任务模板代码结构

--- src/pages/MyData/Warehouse/components/task.tsx
+++ src/pages/MyData/Warehouse/components/task.tsx
@@ -12,27 +12,77 @@ export const SEND_EMAIL = "SEND_EMAIL";
 
 export const TASK_TEMPLATE = {
     API_GET_JSON: {
-        taskType: API_GET_JSON,
-        taskName: "从API获取JSON",
-        taskConfig: {
-            INPUT: {},
-            OUTPUT: { ORIGIN_JSON: "ORIGIN_JSON", DATA_JSON: "DATA_JSON" },
-            BATCH: { ENABLE: false, INTERVAL: 5, END_TYPE: 0, PARAMS: [] },
+        "taskType": API_GET_JSON,
+        "taskName": "从API获取JSON",
+        "taskConfig": {
+            "INPUT": {},
+            "OUTPUT": { "ORIGIN_JSON": "ORIGIN_JSON", "DATA_JSON": "DATA_JSON" },
+            "BATCH": { "ENABLE": false, "INTERVAL": 5, "END_TYPE": 0, "PARAMS": [] },
         },
     },
-    API_SEND_DATA: { taskType: API_SEND_DATA, taskName: "向API发送数据", taskConfig: { INPUT: {}, OUTPUT: {} }, },
-    WEBHOOK_GET_JSON: { taskType: WEBHOOK_GET_JSON, taskName: "从Webhook接收JSON", taskConfig: { INPUT: { WEBHOOK_JSON: "WEBHOOK_JSON" }, OUTPUT: { ORIGIN_JSON: "ORIGIN_JSON", DATA_JSON: "DATA_JSON" } }, },
-    API_GET_VAR: { taskType: API_GET_VAR, taskName: "从API获取参数", taskConfig: { INPUT: {}, OUTPUT: {} }, },
 
-    JSON_TO_DATA: { taskType: JSON_TO_DATA, taskName: "JSON转数据", taskConfig: { INPUT: { DATA_JSON: "DATA_JSON" }, OUTPUT: { BIZ_DATA: "BIZ_DATA", DATA_CODE: "DATA_CODE" } } },
-    FILTER_DATA: { taskType: FILTER_DATA, taskName: "过滤数据", taskConfig: { INPUT: {}, OUTPUT: {} }, },
-    OPERATE_DATA: { taskType: OPERATE_DATA, taskName: "处理数据", taskConfig: { INPUT: {}, OUTPUT: {} }, },
-    WRITE_EXCEL: { taskType: WRITE_EXCEL, taskName: "写入Excel文件", taskConfig: { INPUT: {}, OUTPUT: {} }, },
+    API_SEND_DATA: {
+        "taskType": API_SEND_DATA,
+        "taskName": "向API发送数据",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
 
-    SAVE_DATA: { taskType: SAVE_DATA, taskName: "保存数据到数仓", taskConfig: { INPUT: { BIZ_DATA: "BIZ_DATA" }, OUTPUT: { SAVED_DATA: "SAVED_DATA" } }, },
-    QUERY_DATA: { taskType: QUERY_DATA, taskName: "从数仓查询数据", taskConfig: { INPUT: {}, OUTPUT: {} }, },
+    WEBHOOK_GET_JSON: {
+        "taskType": WEBHOOK_GET_JSON,
+        "taskName": "从Webhook接收JSON",
+        "taskConfig": {
+            "INPUT": { "WEBHOOK_JSON": "WEBHOOK_JSON" },
+            "OUTPUT": { "ORIGIN_JSON": "ORIGIN_JSON", "DATA_JSON": "DATA_JSON" }
+        },
+    },
 
-    SEND_EMAIL: { taskType: SEND_EMAIL, taskName: "发送邮件", taskConfig: { INPUT: {}, OUTPUT: {} }, },
+    API_GET_VAR: {
+        "taskType": API_GET_VAR,
+        "taskName": "从API获取参数",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
+
+    JSON_TO_DATA: {
+        "taskType": JSON_TO_DATA,
+        "taskName": "JSON转数据",
+        "taskConfig": { "INPUT": { "DATA_JSON": "DATA_JSON" }, "OUTPUT": { "BIZ_DATA": "BIZ_DATA", "DATA_CODE": "DATA_CODE" } }
+    },
+
+    FILTER_DATA: {
+        "taskType": FILTER_DATA,
+        "taskName": "过滤数据",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
+
+    OPERATE_DATA: {
+        "taskType": OPERATE_DATA,
+        "taskName": "处理数据",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
+
+    WRITE_EXCEL: {
+        "taskType": WRITE_EXCEL,
+        "taskName": "写入Excel文件",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
+
+    SAVE_DATA: {
+        "taskType": SAVE_DATA,
+        "taskName": "保存数据到数仓",
+        "taskConfig": { "INPUT": { "BIZ_DATA": "BIZ_DATA" }, "OUTPUT": { "SAVED_DATA": "SAVED_DATA" } },
+    },
+
+    QUERY_DATA: {
+        "taskType": QUERY_DATA,
+        "taskName": "从数仓查询数据",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
+
+    SEND_EMAIL: {
+        "taskType": SEND_EMAIL,
+        "taskName": "发送邮件",
+        "taskConfig": { "INPUT": {}, "OUTPUT": {} },
+    },
 }
 
 export type TaskKey = keyof typeof TASK_TEMPLATE;
