be13e92437fbd51ef2fda158eec61349dd38f2d5
LIEN <tle3212000@163.com>
2024-12-07 14:13:34 +0800

    fix: 对已有的任务 补全模板新增的Input、Output参数

--- src/pages/MyData/Warehouse/PipelineTask.tsx
+++ src/pages/MyData/Warehouse/PipelineTask.tsx
@@ -144,10 +144,30 @@ const PipelineTask: React.FC<PipelineTaskProp> = (props) => {
                 if (!task.taskConfig) {
                     task.taskConfig = { ...taskTemplateConfig };
                 } else {
+                    // 补全taskConfig下的第一层属性
+                    const { taskConfig } = task;
                     for (const key in taskTemplateConfig) {
-                        const { taskConfig } = task;
                         if (!taskConfig.hasOwnProperty(key)) {
                             taskConfig[key] = taskTemplateConfig[key as keyof typeof taskTemplateConfig];
+                        }
+                    }
+                    // 补全taskConfig.INPUT 和 taskConfig.OUTPUT 的属性
+                    if(taskTemplateConfig["INPUT"] && task.taskConfig["INPUT"]){
+                        const templateInput = taskTemplateConfig["INPUT"];
+                        const taskInput = task.taskConfig["INPUT"];
+                        for(const key in templateInput){
+                            if(!taskInput.hasOwnProperty(key)){
+                                taskInput[key] = templateInput[key as keyof typeof templateInput];
+                            }
+                        }
+                    }
+                    if(taskTemplateConfig["OUTPUT"] && task.taskConfig["OUTPUT"]){
+                        const templateOutput = taskTemplateConfig["OUTPUT"];
+                        const taskOutput = task.taskConfig["OUTPUT"];
+                        for(const key in templateOutput){
+                            if(!taskOutput.hasOwnProperty(key)){
+                                taskOutput[key] = templateOutput[key as keyof typeof templateOutput];
+                            }
                         }
                     }
                 }
