1075830a6abcaf515d21f290e340477bb3949565
LIEN <tle3212000@163.com>
2024-12-17 18:02:29 +0800

    feat: 数据转JSON的任务 复用前置的数据id

--- src/pages/MyData/Warehouse/PipelineTask.tsx
+++ src/pages/MyData/Warehouse/PipelineTask.tsx
@@ -3,7 +3,7 @@ import { ProCard } from "@ant-design/pro-components";
 import { Button, Card, Col, Dropdown, MenuProps, Popconfirm, Row, Space, Splitter, Typography, theme } from "antd";
 import { useEffect, useState } from "react";
 import PipelineTaskForm from "./PipelineTaskForm";
-import { API_SEND_DATA, FILTER_DATA, PROCESS_DATA, TASK_TEMPLATE, TaskKey } from "../mydata";
+import { API_SEND_DATA, DATA_TO_JSON, FILTER_DATA, PROCESS_DATA, TASK_TEMPLATE, TaskKey } from "../mydata";
 import { DownwardArrowLine } from "../Icons";
 
 export type PipelineTaskProp = {
@@ -193,7 +193,7 @@ const PipelineTask: React.FC<PipelineTaskProp> = (props) => {
         });
 
         // 若新任务是 发送数据、过滤数据 或 处理数据，则从当前新任务前面最近的任务 复制数据id
-        if (type === API_SEND_DATA || type === FILTER_DATA || type === PROCESS_DATA) {
+        if (type === API_SEND_DATA || type === FILTER_DATA || type === PROCESS_DATA || type === DATA_TO_JSON) {
             for (let i = index - 1; i >= 0; i--) {
                 const tmpTask = newTasks[i];
                 if (tmpTask.dataId) {
