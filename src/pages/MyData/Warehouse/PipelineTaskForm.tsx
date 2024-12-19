4148772017313882b703184b8dd2ef0bf4624fb5
LIEN <tle3212000@163.com>
2024-12-19 18:52:58 +0800

    fix: 调用API 增加输入的业务数据可作为参数

--- src/pages/MyData/Warehouse/PipelineTaskForm.tsx
+++ src/pages/MyData/Warehouse/PipelineTaskForm.tsx
@@ -162,6 +162,37 @@ const PipelineTaskForm: React.FC<TaskFormProp> = (props) => {
                         {
                             // ---------------------------------------- 调用API获取JSON ----------------------------------------
                             (task.taskType === API_GET_JSON) && <>
+                                <Row>
+                                    <Col span={24}>
+                                        <ProFormItem label="输入变量" >
+                                            <Row gutter={24}>
+                                                <Col span={2}></Col>
+                                                <Col span={10}>
+                                                    <ProFormText
+                                                        label="作为参数的业务数据"
+                                                        rules={[
+                                                            {
+                                                                required: true,
+                                                                message: '请输入业务数据变量名',
+                                                            }
+                                                        ]}
+                                                        fieldProps={{
+                                                            onChange: (e) => {
+                                                                task.taskConfig.INPUT.BIZ_DATA = e.target.value;
+                                                                updateTask();
+                                                            },
+                                                            value: task.taskConfig.INPUT.BIZ_DATA || "",
+                                                        }}
+                                                    />
+                                                </Col>
+                                                <Col span={2}>
+                                                </Col>
+                                                <Col span={10}>
+                                                </Col>
+                                            </Row>
+                                        </ProFormItem>
+                                    </Col>
+                                </Row>
                                 <Row gutter={24}>
                                     {/* 选择应用 */}
                                     <Col span={12}>
