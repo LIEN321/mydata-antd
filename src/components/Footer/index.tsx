544cd87ac248cf4a635366a7818aa4fb925e40e1
LIEN <tle3212000@163.com>
2025-01-10 13:04:11 +0800

    refactor: v0.9.1

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
           blankTarget: true,
         },
       ]}
-      copyright={`2024 lient321 v0.9.0`}
+      copyright={`2024 lient321 v0.9.1`}
     />
   );
 };
